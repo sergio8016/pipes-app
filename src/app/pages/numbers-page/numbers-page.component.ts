08287b25303514235afa5bd2f6c6acc1c45a5f1e
sergio <sergio8016@gmail.com>
2024-06-27 19:57:11 -0500

    added i18 select and plural pipes

--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {CardModule} from "primeng/card";
+import {PanelModule} from "primeng/panel";
+import {CurrencyPipe, DecimalPipe, PercentPipe, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-numbers-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    PanelModule,
+    UpperCasePipe,
+    DecimalPipe,
+    CurrencyPipe,
+    PercentPipe
+  ],
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.scss'
 })
 export class NumbersPageComponent {
-
+  public totalSells: number = 35345345.3545
+  public percent: number = 0.3545
 }
--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {PanelModule} from "primeng/panel";
+import {FieldsetModule} from "primeng/fieldset";
+import {Button} from "primeng/button";
+import {I18nPluralPipe, I18nSelectPipe} from "@angular/common";
 
 @Component({
   selector: 'app-uncommon-page',
   standalone: true,
-  imports: [],
+  imports: [
+    PanelModule,
+    FieldsetModule,
+    Button,
+    I18nSelectPipe,
+    I18nPluralPipe
+  ],
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.scss'
 })
 export class UncommonPageComponent {
+  public name: string = 'Sergio'
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'guy',
+    'female': 'girl'
+  }
 
+  public clients: string[] = ['Sergio', 'Omaira', 'Francisco', 'Hassan', 'Mateo', 'Isa', 'Abdullah'];
+  public clientsMap: { [k: string]: string } = {
+    '=0': 'no clients waiting.',
+    '=1': 'client waiting.',
+    other: '# clients waiting.'
+  }
+
+  onButtonChangeClick() {
+    this.name = 'Omaira';
+    this.gender = 'female'
+  }
+
+  onDeleteClient() {
+    this.clients.shift();
+  }
 }
